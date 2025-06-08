b624c7ef6512f61bc12deb6d33d3d715d5777e6c
Panharoth06 <panharothchheng0903@gmail.com>
2025-06-08 23:18:33 +0700

    feat: added password encryptor feature

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
 import configuration.DbConnection;
+import org.mindrot.jbcrypt.BCrypt;
+import util.PasswordEncryptor;
 
 import java.sql.Connection;
 
 public class Main {
     public static void main(String[] args) {
         Connection connection = DbConnection.getDatabaseConnection();
+        String password = PasswordEncryptor.hashPassword("koko!@#$@!");
+        System.out.println(password);
+        boolean isMatch = PasswordEncryptor.checkPassword("koko!@#$@!", password);
+        if (isMatch) System.out.println("login successful");
+        else System.out.println("login failed");
+
     }
 }
