f1d4866127d1542408b58ef4f5752a6f8cc35842
Micky <chaingpolin@gmail.com>
2025-06-13 12:28:59 +0700

    finish login and register

--- src/configuration/DbConnection.java
+++ src/configuration/DbConnection.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DbConnection {
     private static final String dbUrl = "jdbc:postgresql://localhost:5432/e_commerce";
-    private static final String dbPassword = "qwer";
+    private static final String dbPassword = "1234";
     private static final String dbUsername = "postgres";
 
     public static Connection getDatabaseConnection() {
